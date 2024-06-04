8ab6857a4f360fe2f2228a939ae6e0a044d99ef4
Victtor777 <victtor.eng.software@gmail.com>
2024-06-04 16:03:45 -0300

    Configurante o layout no react-router-dom

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/Home";
 import { CartPage } from "./pages/Cart";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/cart" element={<CartPage />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Route>
     </Routes>
   );
 }
