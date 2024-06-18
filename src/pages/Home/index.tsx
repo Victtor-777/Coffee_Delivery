1d9ba78437fbe41f9e8bb3b98b7681e8f4aa12d1
Victtor777 <victtor.eng.software@gmail.com>
2024-06-18 19:07:25 -0300

    Add Context to project

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
+import { useCart } from "../../hooks/useCart";
 import { Coffees } from "./Components/Coffees";
 import { Hero } from "./Components/Hero";
 
 export function HomePage() {
+  const { cartItems } = useCart();
+
   return (
     <div>
       <Hero />
