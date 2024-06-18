4801d7aae7d2aefbfccd74e7581376b25975e2c2
Victtor777 <victtor.eng.software@gmail.com>
2024-06-18 20:17:10 -0300

    Add função de adicionar ou remover mais cafés do carrinho

--- src/pages/Cart/Components/CoffeeCartCard/index.tsx
+++ src/pages/Cart/Components/CoffeeCartCard/index.tsx
@@ -8,6 +8,7 @@ import {
 } from "./styles";
 import { CartItem } from "../../../../contexts/CartContext";
 import { formatMoney } from "../../../../utils/formatMoney";
+import { useCart } from "../../../../hooks/useCart";
 
 interface CoffeeCartCardProps {
   coffee: CartItem;
@@ -16,6 +17,15 @@ interface CoffeeCartCardProps {
 export function CoffeeCartCard({ coffee }: CoffeeCartCardProps) {
   const coffeeTotal = coffee.price * coffee.quantity;
   const formattedPrice = formatMoney(coffeeTotal);
+  const { changeCartItemQuantity } = useCart();
+
+  function handleIncrease() {
+    changeCartItemQuantity(coffee.id, "increase");
+  }
+
+  function handleDecrease() {
+    changeCartItemQuantity(coffee.id, "decrease");
+  }
 
   return (
     <CoffeeCartCardContainer>
@@ -24,7 +34,12 @@ export function CoffeeCartCard({ coffee }: CoffeeCartCardProps) {
         <div>
           <RegularText color="subtitle">{coffee.title}</RegularText>
           <ActionsContainer>
-            <QuantityInput size="small" />
+            <QuantityInput
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+              size="small"
+              quantity={coffee.quantity}
+            />
             <RemoveButton>
               <Trash size={16} />
               REMOVER
